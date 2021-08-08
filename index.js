7f7acb7c599fc9bd71856c9429886d9b8f53c524
Gabrielradtke <gabriel_radtke@hotmail.com>
2021-08-08 18:35:56 -0300

    Conclusão página principal
    - Atualização do layout principal;
    - Conclusão das funcionalidades principais, na parte de cálculos;
    - Ajuste nas páginas extras com a mensagem de "Manutenção"

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
 const pegarData = document.querySelector('#start')
 const button = document.querySelector('button')
-const span = document.querySelector('span.teste')
+const divAnt = document.querySelector('#resAnt')
 
-
-function calcularData() {
+// DATA ANTERIOR
+function calcularDataAnterior() {
     let start = pegarData.value
     novostart = new Date(start)
-
+    
     let ndt = novostart
-    resultado = ndt.setDate(novostart.getDate() + 10)
-
+    resultado = ndt.setDate(novostart.getDate() - 310)
+    
     resultadoData = new Date(resultado)
-
+    
     return resultadoData
 }
 
-const dataFormatada = calcularData();
-
+const dataFormatadaAnterior = calcularDataAnterior();
 const formatter = Intl.DateTimeFormat('pt-BR', {
     dataStyle: 'short',
 });
 
 button.addEventListener('click', () => {
-    span.innerHTML = formatter.format(calcularData());
+    divAnt.innerHTML = formatter.format(calcularDataAnterior());
+});
+
+// DATA POSTERIOR 
+const pegarDataPosterior = document.querySelector('#startPosterior')
+const button2 = document.querySelector('#button2')
+const divPos = document.querySelector('#resPos')
+
+function calcularDataPosterior(){
+    let startPosterior = pegarDataPosterior.value
+    novostartPosterior = new Date(startPosterior)
+
+    let ndtPosterior = novostartPosterior
+    resultado2 = ndtPosterior.setDate(novostartPosterior.getDate() + 312)
+
+    resultadoDataPosterior = new Date(resultado2)
+
+    return resultadoDataPosterior
+}
+
+const dataFormatadaPosterior = calcularDataPosterior();
+const formatter2 = Intl.DateTimeFormat('pt-BR', {
+    dataStyle: 'short',
+});
+
+button2.addEventListener('click', () => {
+    divPos.innerHTML = formatter2.format(calcularDataPosterior());
 });
 
 // Pagina FAQ
-
-/**
- * Created by kgugec on 15-Sep-17.
- */
